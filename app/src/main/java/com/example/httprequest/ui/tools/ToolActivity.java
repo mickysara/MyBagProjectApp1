3e2d7bc8e57b4a0a88513caef330263d080514e9
Harmonic <buygameraidee@gmail.com>
2020-03-07 23:40:40 +0700

    not ok

--- app/src/main/java/com/example/httprequest/ui/tools/ToolActivity.java
+++ app/src/main/java/com/example/httprequest/ui/tools/ToolActivity.java
@@ -6,17 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.httprequest.R;
 
-public class ToolActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.contentMainFragment, new ToolsFragment()).commit();
-        }
-
-    }
-}
+//public class ToolActivity extends AppCompatActivity  {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.contentMainFragment, new ToolsFragment()).commit();
+//        }
+//
+//    }
+//}
--- app/src/main/java/com/example/httprequest/ui/tools/ToolsFragment.java
+++ app/src/main/java/com/example/httprequest/ui/tools/ToolsFragment.java
@@ -1,6 +1,9 @@
 package com.example.httprequest.ui.tools;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +16,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -22,18 +27,62 @@ import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
+import static com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class ToolsFragment extends Fragment {
+
+public class ToolsFragment extends Fragment  implements ConnectionCallbacks, OnConnectionFailedListener {
+
+    public static final int RequestPermissionCode = 1;
+    protected GoogleApiClient googleApiClient;
+    protected TextView longitudeText;
+    protected TextView latitudeText;
+    protected Location lastLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
     private ZXingScannerView zXingScannerView;
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 
         super.onActivityCreated(savedInstanceState);
 
+
+        googleApiClient = new GoogleApiClient.Builder(getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
         Button button = getView().findViewById(R.id.btnQRscan);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+//                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA)
+//                        == PackageManager.PERMISSION_DENIED)
+//                {
+//                    requestPermissions(new String[] {Manifest.permission.CAMERA}, REQUEST_CODE);
+//                    return;
+//                }
+
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED )
+                {
+                    requestPermissions( new String[]{
+
+                            Manifest.permission.CAMERA}, 225);
+                    return;
+                }
+
 
                 zXingScannerView = new ZXingScannerView(getActivity());
                 getActivity().setContentView(zXingScannerView);
@@ -62,5 +111,20 @@ public class ToolsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tools, container, false);
         return view;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
